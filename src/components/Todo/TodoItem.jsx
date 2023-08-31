74bb95e28140861ed2a17fc1cde6437115721464
inferizer <codecamp.1412@gmail.com>
2023-08-31 16:22:13 +0700

    Refactor:TodoItem

---
+++ src/components/Todo/TodoItem.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { FaTrashAlt, FaPen } from "react-icons/fa";
+import { HiOutlineCheck } from "react-icons/hi";
+
+import TodoForm from "./TodoForm";
+import styles from "./TodoItem.module.scss";
+
+function TodoItem() {
+  const [isOpenForm, setIsOpenForm] = useState(false);
+
+  const hdlClick = () => {
+    setIsOpenForm(!isOpenForm);
+  };
+  return (
+    <>
+      {isOpenForm ? (
+        <TodoForm textSubmit='Edit Task' setIsOpenForm={setIsOpenForm} />
+      ) : (
+        <ul className={styles.todo__lists}>
+          <li className={styles.todo}>
+            <div
+              className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}
+            >
+              <HiOutlineCheck className={styles.todo__checkbox__icon} />
+            </div>
+            <p className={`${styles.todo__task} ${styles.todo__task__done}`}>
+              todo-item 1{" "}
+            </p>
+            <span className={styles.todo__date}>
+              30 AUG
+              {/* {today.toLocaleDateString("en-US", options)} */}
+            </span>
+            <div className={styles.todo__action}>
+              <span onClick={hdlClick}>
+                <FaPen className={styles.todo__edit} />
+              </span>
+              <span>
+                <FaTrashAlt className={styles.todo__delete} />
+              </span>
+            </div>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default TodoItem;
