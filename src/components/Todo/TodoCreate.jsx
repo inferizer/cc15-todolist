e20a6ef89cd303b17d7c38a0b94e36fdc310fd32
inferizer <codecamp.1412@gmail.com>
2023-08-31 11:05:06 +0700

    biding state OpenForm with btn

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,53 @@
-import styles from './TodoCreate.module.scss';
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
+import { useState } from 'react';
 
+import styles from './TodoCreate.module.scss';
+import TodoForm from './TodoForm';
+
+/* 
+Condition Rendering
+- Default : Show btn & txt
+- Active : Show TodoForm
+ */
+
+// SEC1 - Concept : true ? <AddTask/> : <TodoForm/>
+
+/* SEC2 - Evt Hdl 
+    - Binding with UI
+    - onClick : wait user click to run FN
+        # 1. User click
+        # 2. Browser call FN
+          Browser send param "EventOBJ" to FN : hdlClick(EventOBJ)
+*/
+
+/* 
+SEC3 - useState, because JS Value can't Render React
+      React State (React Hook)
+      const [state,setState] = useState(true) 
+
+*/
+
+// # FC : Render
 function TodoCreate() {
+  // HOOK FN
+   const [isOpenForm,setIsOpenForm] = useState(false);
+  // # JSF : Logic
+  const hdlClick = () => {
+      setIsOpenForm(!isOpenForm)
+      // setState value = opposite of state
+  }
+
   return (
-    <div className={styles.todo__create}>
+    <>
+    {isOpenForm ? (<TodoForm/>) :
+    (<div className={styles.todo__create} onClick={hdlClick}>
       <div className={styles.todo__create__button}>
-        <HiPlus />
+      <HiPlus />
       </div>
       <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    </div>)}
+    </>
   );
 }
 
